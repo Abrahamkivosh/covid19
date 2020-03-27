a55ce6260adf8c495045ba50736bb614cc455bc2
yulemsee@github.com <yulemsee@github.com>
2020-03-27 13:11:39 -0700

    Added English Home page

---
+++ app/src/main/java/com/paviham/covid19/EnglishHome.java
@@ -0,0 +1,14 @@
+package com.paviham.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnglishHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_home);
+    }
+}
